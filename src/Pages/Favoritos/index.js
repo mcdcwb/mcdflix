3da7193bce69817ef86b497b5a31908a7fc3593d
Luan Machado <machadoluaan@gmail.com>
2025-05-22 18:31:24 -0300

    att

--- src/Pages/Favoritos/index.js
+++ src/Pages/Favoritos/index.js
@@ -31,13 +31,13 @@ function Favoritos() {
         <div className="meus-filmes">
             <h1>Meus Favoritos</h1>
 
-            {filmes.length === 0 && <span className="spam-empty">Você não possui nenhum filme salvo no momento. <img width="100" height="100" src="https://img.icons8.com/quill/100/f26c4f/sad.png" alt="sad"/></span>}
+            {filmes.length === 0 && <span className="span-empty">Você não possui nenhum filme salvo no momento. <img width="100" height="100" src="https://img.icons8.com/quill/100/f26c4f/sad.png" alt="sad"/></span>}
 
             <ul>
                 {filmes.map((item) => {
                     return(
                         <li key={item.id}>
-                            <span>{item.title}</span>
+                            <span className="span-title">{item.title}</span>
                             <div>
                                 <Link className="item-li" to={`/filme/${item.id}`}>Ver detalhes</Link>
                                 <button onClick={() => excluirFilme(item.id) }>Excluir</button>
